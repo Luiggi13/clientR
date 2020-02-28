69850130dade9a9f190222d60f018936014597b7
Christian Llansola <cllansola@strategying.com>
2020-02-28 14:24:19 +0100

    Routes with layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import Admin from "./pages/Admin"
-import SignIn from "./pages/Admin/SignIn"
-import Home from "./pages/Home"
-import Contact from "./pages/Contact"
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import routes from "./config/routes";
 import './App.scss';
 
 function App() {
 
   return (
-    <div>
-      <h1>Estamos en App.js</h1>
-      <Admin />
-      <SignIn />
-      <Home />
-      <Contact />
-    </div>
+    <Router>
+      <Switch>
+        { routes.map((route, index) => (
+          <RouteWithSubRoutes key={index} {...route} />
+        ))}
+      </Switch>
+    </Router>
   );
+}
+function RouteWithSubRoutes(route) {
+  console.log(route);
+  return (
+    <Route 
+    path={route.path}
+    exact={route.exact}
+    render={props => <route.component routes={route.routes} {...props} />}
+    />
+  )
 }
 
 
