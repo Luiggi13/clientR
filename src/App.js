2786f5b429e8fe59342a73c79745082111e86e71
Christian Llansola <cllansola@strategying.com>
2020-02-28 13:26:56 +0100

    Pages structure

--- src/App.js
+++ src/App.js
@@ -1,38 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router , Route, Link, Switch  } from "react-router-dom"
+import Admin from "./pages/Admin"
+import SignIn from "./pages/Admin/SignIn"
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
 import './App.scss';
 
 function App() {
 
   return (
-    <Router>
-      <div className="app">
-        <h1>Sistema de rutas base</h1>
-        <Link to="/">Home</Link><br />
-        <Link to="/contact">Contact</Link><br />
-        <Link to="/users">Users</Link>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/contact" exact component={Contact} />
-          <Route path="/users" exact component={Users} />
-          <Route component={Error404} />
-        </Switch>
-      </div>
-    </Router>
+    <div>
+      <h1>Estamos en App.js</h1>
+      <Admin />
+      <SignIn />
+      <Home />
+      <Contact />
+    </div>
   );
 }
 
-function Home() {
-  return <h2>Estamos en el componente Home ...</h2>;
-}
-function Contact() {
-  return <h2>Estamos en el componente Contact...</h2>;
-}
-function Users() {
-  return <h2>Estamos en el componente Users ...</h2>;
-}
-function Error404() {
-  return <h2>404: Página no encontrada</h2>;
-}
 
 export default App;
