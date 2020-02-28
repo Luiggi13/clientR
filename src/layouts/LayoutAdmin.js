b72960f428bfcb9dd2846b9b43a3257e15de7b57
Christian Llansola <cllansola@strategying.com>
2020-02-28 13:44:32 +0100

    Base layouts

---
+++ src/layouts/LayoutAdmin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Layout} from 'antd';
+// const { Header, Footer, Sider, Content } = Layout;
+
+export default function LayoutAdmin() {
+    return (
+        <Layout>
+            <h2>Menu sidebar</h2>
+            <div>Contenido....</div>
+            <h5>Footer</h5>
+        </Layout>
+
+    )
+}
---
+++ src/layouts/LayoutBasic.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Layout} from 'antd';
+
+export default function LayoutBasic() {
+    return (
+        <Layout>
+            <h2>Menu sidebar</h2>
+            <div>Contenido....</div>
+            <h5>Footer</h5>
+        </Layout>
+
+    )
+}
