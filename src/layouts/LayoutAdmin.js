6134eac4b250bd16f5874f68c01036ad60d95f3c
Christian Llansola <cllansola@strategying.com>
2020-02-28 20:28:08 +0100

    LayoutAdmin with conditional,for child pages

--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import {Layout} from 'antd';
+import { Route } from 'react-router-dom';
+import { Layout } from 'antd';
+
+import "./LayoutAdmin.scss"
 // const { Header, Footer, Sider, Content } = Layout;
 
-export default function LayoutAdmin() {
+export default function LayoutAdmin(props) {
+    const { Header, Content, Footer} = Layout;
+    const { routes } = props; 
+    // console.log(props);     
+
     return (
         <Layout>
             <h2>Menu sidebar</h2>
-            <div>Contenido....</div>
-            <h5>Footer</h5>
+            <Layout>
+                <Header className="header__layout--admin">Header</Header>
+                <Content>
+                    <LoadRouters routes= {routes} />
+                </Content>
+                <Footer>Christian 2019</Footer>
+            </Layout>
         </Layout>
 
     )
+}
+
+function LoadRouters({routes}) {
+
+    return routes.map((route,index) => (
+        <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        component={route.component}
+        />
+    ));
 }
