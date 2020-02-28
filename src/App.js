40672a85260c228bfbeeb469d7a2b8ff5335cca0
Christian Llansola <cllansola@strategying.com>
2020-02-28 13:04:07 +0100

    React router added and links with 404

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-
+import { BrowserRouter as Router , Route, Link, Switch  } from "react-router-dom"
 import './App.scss';
 
 function App() {
 
-  const test = (date, dateString) => {
-    console.log(date, dateString);
-  }
-
   return (
-    <div className="app">
-     <h1>Mi web personal</h1>
-     <h2>Proyecto personal</h2>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Sistema de rutas base</h1>
+        <Link to="/">Home</Link><br />
+        <Link to="/contact">Contact</Link><br />
+        <Link to="/users">Users</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/users" exact component={Users} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Estamos en el componente Home ...</h2>;
+}
+function Contact() {
+  return <h2>Estamos en el componente Contact...</h2>;
+}
+function Users() {
+  return <h2>Estamos en el componente Users ...</h2>;
+}
+function Error404() {
+  return <h2>404: Página no encontrada</h2>;
 }
 
 export default App;
