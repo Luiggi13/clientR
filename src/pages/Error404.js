2536c944223b1ee98d13e4e78c2db1599323d702
Christian Llansola <cllansola@strategying.com>
2020-03-16 18:05:00 +0100

    modified 404 styles

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import "./Error404.scss"
+
 export default function Error404 () {
     return(
         <div>
-            <h1>estamos en error 404</h1>
+            <h1>La página que estás buscando no existe</h1>
+            <h2 className="error-404">Error 404</h2>
         </div>
     )
 }
