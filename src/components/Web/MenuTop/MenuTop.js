f4d02afc6b076bd6f6b5ed00cb2595e50567573d
Christian Llansola <cllansola@strategying.com>
2020-03-18 00:55:29 +0100

    get menu top from back, and render internal links, and external

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { Menu } from "antd";
+import { getMenuApi } from "../../../api/menu";
 import { Link } from "react-router-dom";
 import LogoWeb from "../../../assets/img/png/cllansola.png"
 
 import "./MenuTop.scss";
 
 export default function MenuTop() {
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        getMenuApi().then(response => {
+            const arrayMenu = [];
+            response.menu.forEach(item => {
+                item.active && arrayMenu.push(item);
+            });
+            setMenuData(arrayMenu);
+        })
+    },[])
     return(
-        <Menu className="menu-top" mode="horizontal">
-            <Menu.Item className="menu-top__logo">
+        <Menu className="menu-top-web" mode="horizontal">
+            <Menu.Item className="menu-top-web__logo">
             <Link to={"/"}>
                 <img
                 src={LogoWeb}
@@ -16,16 +28,33 @@ export default function MenuTop() {
                 />
                 </Link>
             </Menu.Item>
-            <Menu.Item className="menu-top__item">
+            { menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false;
+                if(external) {
+                    return (
+                        <Menu.Item key={item._id} className="menu-top-web__item">
+                            <a href={item.url} target="_blank" rel="noopener noreferrer">{item.title}</a>
+                        </Menu.Item>
+                    )
+                }
+                return (
+                    <Menu.Item key={item._id} className="menu-top-web__item">
+                        <Link to={item.url}>
+                            {item.title}
+                        </Link>
+                    </Menu.Item>
+                )
+            })}
+            {/* <Menu.Item className="menu-top-web__item">
                 <Link to={"/"}>
                     Home
                 </Link>
             </Menu.Item>
-            <Menu.Item className="menu-top__item">
+            <Menu.Item className="menu-top-web__item">
                 <Link to={"/contact"}>
                     Contacto
                 </Link>
-            </Menu.Item>
+            </Menu.Item> */}
             <div>
                 RRSS
             </div>
