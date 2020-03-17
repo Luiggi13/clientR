75cb37f226c8c979e2da75e7de556e6a8f79b5ea
Christian Llansola <cllansola@strategying.com>
2020-03-17 23:18:05 +0100

    added LogoWeb on topMenu

--- src/components/Web/MenuTop/MenuTop.js
+++ src/components/Web/MenuTop/MenuTop.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Menu } from "antd";
 import { Link } from "react-router-dom";
+import LogoWeb from "../../../assets/img/png/cllansola.png"
 
 import "./MenuTop.scss";
 
@@ -8,7 +9,12 @@ export default function MenuTop() {
     return(
         <Menu className="menu-top" mode="horizontal">
             <Menu.Item className="menu-top__logo">
-                Logo...
+            <Link to={"/"}>
+                <img
+                src={LogoWeb}
+                alt="Christian Llansola"
+                />
+                </Link>
             </Menu.Item>
             <Menu.Item className="menu-top__item">
                 <Link to={"/"}>
