c3713ea2d52565aba3dee9d49eb17c92092b4caf
Christian Llansola <cllansola@strategying.com>
2020-03-17 13:43:35 +0100

    endpoint to update item from drag and drop

--- src/api/menu.js
+++ src/api/menu.js
@@ -14,3 +14,28 @@ export function getMenuApi() {
       return err.message;
     });
 }
+
+export function updateMenuApi(token, menuId, data) {
+    const url = `${BASE_PATH}/${API_VERSION}/update-menu/${menuId}`;
+
+    const params = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    },
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result.message;
+    })
+    .catch(err => {
+      return err;
+    });
+
+}
