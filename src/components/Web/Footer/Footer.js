5d2476d3ad294a0ff68dd2652c637831ac5b4f5c
Christian Llansola <cllansola@strategying.com>
2020-03-18 15:53:50 +0100

    added footer component , layout and some styles

---
+++ src/components/Web/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Layout, Row, Col } from "antd";
+// import { ClockCircleOutlined, KeyOutlined, MessageOutlined, UserOutlined, DollarOutlined, CheckCircleOutlined} from '@ant-design/icons';
+
+import "./Footer.scss";
+export default function Footer() {
+    const { Footer } = Layout;
+    return (
+        <Footer className="footer">
+            <Row>
+                <Col md={4} />
+                <Col md={16}>
+                    <Row>
+                        <Col md={8}>Mi info</Col>
+                        <Col md={8}>Navegación</Col>
+                        <Col md={8}>newsletter</Col>
+                    </Row>
+                    <Row className="footer__copyright">
+                        <Col md={12}>© 2019 ALL RIGHTS RESERVED</Col>
+                        <Col md={12}>Christian Llansola Llopis | Front End Dev.</Col>
+                    </Row>
+                </Col>
+                <Col md={4} />
+            </Row>
+        </Footer>
+    )
+}
