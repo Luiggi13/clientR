3c96568cd01b368f1a636de7713b146aa5ae76b1
Christian Llansola <cllansola@strategying.com>
2020-03-19 15:26:26 +0100

    newsletter component

--- src/components/Web/Footer/Footer.js
+++ src/components/Web/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Layout, Row, Col } from "antd";
 import MyInfo from "./MyInfo";
 import NavigationFooter from "./NavigationFooter";
+import Newsletter from "../Newsletter";
 import "./Footer.scss";
 export default function Footer() {
     const { Footer } = Layout;
@@ -13,7 +14,7 @@ export default function Footer() {
                     <Row>
                         <Col md={8}><MyInfo /></Col>
                         <Col md={8}><NavigationFooter /></Col>
-                        <Col md={8}>newsletter</Col>
+                        <Col md={8}><Newsletter /></Col>
                     </Row>
                     <Row className="footer__copyright">
                         <Col md={12}>© 2019 ALL RIGHTS RESERVED</Col>
