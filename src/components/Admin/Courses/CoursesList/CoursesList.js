7c5025b96e74ae14d9db14e6dceda4193c6467b3
Christian Llansola <cllansola@strategying.com>
2020-03-19 21:24:35 +0100

    Admin Courses && courses list

---
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CoursesList(props) {
+    const { courses, setReloadCourses} = props;
+    console.log(props);
+    
+    return (
+        <div>
+            <h1>Courses list</h1>
+        </div>
+    )
+}
--- src/pages/Admin/Courses.js
+++ src/pages/Admin/Courses.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import CoursesList from "../../components/Admin/Courses/CoursesList";
+import { getCoursesApi } from "../../api/course";
 export default function Courses() {
+    const [courses, setCourses] = useState([]);
+    const [reloadCourses, setReloadCourses] = useState(false);
+
+    useEffect(() => {
+        getCoursesApi().then(response => {
+            setCourses(response.courses);
+        })
+        setReloadCourses(false);
+    }, [reloadCourses])
+
     return (
         <div className="courses">
-            <h1>Administrar cursos</h1>
+            <CoursesList courses={courses} setReloadCourses={setReloadCourses} />
         </div>
     )
 }
