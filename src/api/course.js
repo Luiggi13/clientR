f689e30c40eb83e118895f5d5f156b074f34e1bb
Christian Llansola <cllansola@strategying.com>
2020-03-20 10:45:28 +0100

    function to retrieve data courses from Udemy

--- src/api/course.js
+++ src/api/course.js
@@ -16,3 +16,22 @@ export function getCoursesApi() {
     });
 
 }
+
+export function getCourseDataUdemyApi(id){
+
+    const baseUrl = `https://www.udemy.com/api-2.0/courses/${id}`;
+    const coursesParams = "?fields[course]=title,headline,url,price,image_480x270";
+    const  url = baseUrl + coursesParams;
+
+    return fetch(url)
+    .then(async response => {
+        return {code: response.status, data: await response.json()}
+    })
+    .then(result => {
+        return result;
+    })
+    .catch(err => {
+        return err;
+    });
+
+}
