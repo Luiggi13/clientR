67162d45c111b5460a8e7974343c70220e016a93
Christian Llansola <cllansola@strategying.com>
2020-03-20 10:45:50 +0100

    basic function to get data from Udemy

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { getCourseDataUdemyApi } from "../../../../api/course";
 export default function CoursesList(props) {
     const { courses, setReloadCourses} = props;
-    console.log(props);
+
+    if (courses.length > 0) {
+        courses.forEach(course => {
+            getCourseDataUdemyApi(course.idCourse).then( response => {
+                console.log(response);
+                
+            })
+        });
+        
+    }
     
     return (
         <div>
