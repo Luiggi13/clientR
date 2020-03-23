f23ad87642529ab2e4dca88b7d3d166830c5d144
Christian Llansola <cllansola@strategying.com>
2020-03-23 13:25:07 +0100

    Added CousesList for Front

---
+++ src/components/Web/Courses/CoursesList/CoursesList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import "./CoursesList.scss";
+export default function CoursesList() {
+    return (
+        <div>
+            <h2>CoursesList</h2>
+        </div>
+    )
+}
