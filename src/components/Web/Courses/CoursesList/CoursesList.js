9ae91c2b899599aa7db99de40be4a862a2ccf8ca
Christian Llansola <cllansola@strategying.com>
2020-03-23 18:28:37 +0100

    cards for courses with styles

--- src/components/Web/Courses/CoursesList/CoursesList.js
+++ src/components/Web/Courses/CoursesList/CoursesList.js
@@ -10,9 +10,9 @@ export default function CoursesList(props) {
         <div className="courses-list">
             <Row>
                 {courses.map(course => (
-                <Col md={8} className="courses-list__course"  key={course._id}>
-                    <Course course={course} />
-                </Col>
+                    <Col md={8} className="courses-list__course" key={course._id}>
+                        <Course course={course} />
+                    </Col>
                 ))}
             </Row>
         </div>
@@ -23,28 +23,43 @@ export default function CoursesList(props) {
 function Course(props) {
     const { course } = props;
     const [courseInfo, setCourseInfo] = useState({});
-console.log(courseInfo);
-
-useEffect(() => {
-    getCourseDataUdemyApi(course.idCourse)
-    .then(response => {
-        if (response?.code !== 200) {
-            notification['warning']({
-                message: response.message
+    const { Meta } = Card;
+console.log(courseInfo)
+    useEffect(() => {
+        getCourseDataUdemyApi(course.idCourse)
+            .then(response => {
+                if (response?.code !== 200) {
+                    notification['warning']({
+                        message: response.message
+                    })
+                } else {
+                    setCourseInfo(response.data)
+                }
             })
-        } else {
-            setCourseInfo(response.data)
-        }
-    })
-    .catch(() => {
-        notification['error']({
-            message: "error del servidor, inténtelo más tarde."
-        })
-    })
-}, [course])
-    return (<>
-        <h1>{course.idCourse}</h1>
-        <h2>{course.link}</h2>
-        </>
+            .catch(() => {
+                notification['error']({
+                    message: "error del servidor, inténtelo más tarde."
+                })
+            })
+    }, [course])
+    return (
+        <a href="http://www.google.es" target="_blank" alt="asd" rel="noopener noreferrer">
+            <Card
+            cover={<img src={courseInfo.image_480x270} alt={courseInfo.title} />}
+            >
+                <Meta
+                title={courseInfo.title}
+                description={courseInfo.headline}
+                />
+                <Button>Entrar a curso</Button>
+                <div className="courses-list__course-footer">
+                    <span>{course.price ? `${course.price} €`: courseInfo.price}</span>
+                    <div>
+                        <Rate defaultValue={5} />
+                    </div>
+                </div>
+            </Card>
+        </a>
+
     )
 }
