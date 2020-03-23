d27f1ee46725398585db14fef176de3665769e07
Christian Llansola <cllansola@strategying.com>
2020-03-23 13:25:35 +0100

    Added components abour Courses

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { Row, Col, Spin, notification } from "antd";
+import { getCoursesApi } from "../api/course";
+import PresentationCourses from "../components/Web/Courses/PresentationCourses";
+import CoursesList from "../components/Web/Courses/CoursesList";
 
 export default function Courses() {
     return (
-        <div className="courses">
-            <h1>Courses</h1>
-        </div>
+        <Row className="courses">
+            <Col md={4}/>
+            <Col md={16}>
+                <PresentationCourses />
+                <CoursesList />
+            </Col>
+            <Col md={4}/>
+        </Row>
     )
 }
