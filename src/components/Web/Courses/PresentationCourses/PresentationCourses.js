adb915d451ee07039f66846e05a977d9abbd7e6f
Christian Llansola <cllansola@strategying.com>
2020-03-23 13:33:11 +0100

    component PresentationCourses finished

--- src/components/Web/Courses/PresentationCourses/PresentationCourses.js
+++ src/components/Web/Courses/PresentationCourses/PresentationCourses.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import AcademyLogo from '../../../../assets/img/png/academy-logo.png';
 import "./PresentationCourses.scss";
 export default function PresentationCourses() {
     return (
-        <div>
-            <h2>PresentationCourses</h2>
+        <div className="presentation-courses">
+            <img src={AcademyLogo} alt="Cursos Udemy" />
+            <p>Id quis aliquip proident quis ut id ipsum occaecat in nostrud ad id. Esse amet aliquip aliquip est ullamco excepteur enim nostrud adipisicing incididunt consectetur. Consectetur nisi sint velit tempor magna aliquip esse reprehenderit. Duis enim laboris quis minim aliqua dolor consectetur. Aliqua veniam magna consectetur incididunt aliqua est commodo in est aliqua amet. Non mollit deserunt esse duis eu nisi. Aliquip non ut anim proident.</p>
+            <p>Ea minim velit in velit exercitation esse ex enim.</p>
         </div>
     )
 }
