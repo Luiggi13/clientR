55ef43c1c891088cf0d9e019cfbcd0ff0f3ccfb4
Christian Llansola <cllansola@strategying.com>
2020-03-23 14:00:27 +0100

    get Courses from backend

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,18 +1,50 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col, Spin, notification } from "antd";
 import { getCoursesApi } from "../api/course";
 import PresentationCourses from "../components/Web/Courses/PresentationCourses";
 import CoursesList from "../components/Web/Courses/CoursesList";
 
 export default function Courses() {
+    const [courses, setCourses] = useState(null);
+    
+    useEffect(() => {
+        getCoursesApi()
+            .then( response => {
+                if(response?.code !== 200) {
+                    notification['warning']({
+                        message: response.message
+                    });
+                } else {
+                    setCourses(response.courses);
+                }
+        })
+        .catch((err)=> {
+            notification['error']({
+                message: "Error del servidor"
+            })
+        });
+    }, [])
     return (
         <Row className="courses">
             <Col md={4}/>
             <Col md={16}>
                 <PresentationCourses />
-                <CoursesList />
+                {!courses ? (
+            <Spin
+              tip="Cargando cursos"
+              style={{ textAlign: "center", width: "100%", padding: "20px" }}
+            />
+          ) : (
+            <CoursesList courses={courses} />
+          )}
             </Col>
             <Col md={4}/>
         </Row>
     )
 }
+
+const showSpin = (e) => {
+return <Spin />
+}
+
+
