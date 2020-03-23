9970a6b642b182270891f903f84978829f363d5d
Christian Llansola <cllansola@strategying.com>
2020-03-23 23:51:36 +0100

    basic layout blog

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Button, notification } from "antd";
+import Modal from "../../../components/Modal";
+
 
 import "./Blog.scss";
 export default function Blog() {
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
+
     return (
-        <div>
-            <h1>blog admin</h1>
+        <div className="blog">
+            <div className="blog__add-post">
+                <Button type="primary">
+                    Nuevo post
+                </Button>
+            </div>
+            <h1>Post list</h1>
+            <h2>PAginación</h2>
+            <Modal
+                title={modalTitle}
+                isVisible={isVisibleModal}
+                setIsVisible={setIsVisibleModal}
+                width="75%"
+            />
         </div>
     )
 }
