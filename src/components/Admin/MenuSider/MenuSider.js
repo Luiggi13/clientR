edc80965f51a6a3db28e0a5740346adad38d5c92
Christian Llansola <cllansola@strategying.com>
2020-03-23 23:14:14 +0100

    Add Blog to the sidebar

--- src/components/Admin/MenuSider/MenuSider.js
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 import {Layout, Menu} from "antd"
-import { BookOutlined,UserOutlined, HomeOutlined, UnorderedListOutlined } from '@ant-design/icons';
+import { BookOutlined,UserOutlined, HomeOutlined, UnorderedListOutlined, MessageOutlined } from '@ant-design/icons';
 
 import "./MenuSider.scss"
 
@@ -39,6 +39,12 @@ function MenuSider(props) {
                         <Link to={"/admin/courses"}>
                         <BookOutlined />
                             <span className="nav-text">Cursos</span>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="/admin/blog">
+                        <Link to={"/admin/blog"}>
+                        <MessageOutlined />
+                            <span className="nav-text">Blog</span>
                         </Link>
                     </Menu.Item>
                 </Menu>
