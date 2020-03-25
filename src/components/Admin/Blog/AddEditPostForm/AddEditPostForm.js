4d6966a6fc47288f8b672d03cbb5922564fc1417
Christian Llansola <cllansola@strategying.com>
2020-03-25 13:39:04 +0100

    added AddEditPostForm component for admin

---
+++ src/components/Admin/Blog/AddEditPostForm/AddEditPostForm.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import { Row, Col, Form, Input, Button, DatePicker, notification } from "antd";
+import { EditOutlined } from '@ant-design/icons';
+import { Editor } from "@tinymce/tinymce-react";
+
+import "./AddEditPostForm";
+
+export default function AddEditPostForm(props) {
+    return (
+        <div>
+            <h1>AddEditPostForm</h1>
+        </div>
+    )
+}
